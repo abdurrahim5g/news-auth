b66e1e2742e996425ce6abde6632a239e1b0544f
f-rahim <freelancerrahim26@gmail.com>
2023-05-28 11:46:11 +0600

    React project setup with _react-bootstrap _bootstrap _firebase _react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Col, Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container>
+      <Col lg="12">
+        <h2>Basic project setup done</h2>
+      </Col>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCS9vKbS3f4tPMPytSdrIU6Oy3vzg2cKTQ",
+  authDomain: "news-auth-9f3a9.firebaseapp.com",
+  projectId: "news-auth-9f3a9",
+  storageBucket: "news-auth-9f3a9.appspot.com",
+  messagingSenderId: "905770170131",
+  appId: "1:905770170131:web:d0eee9b159a42d9c97cce6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
